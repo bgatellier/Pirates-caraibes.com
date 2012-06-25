8d710c42915b23a734f18b2315a218a6c335ee89
Bastien Gatellier <contact@bgatellier.fr>
2012-06-25 22:51:34 +0200

    Push to Git, yay !

---
+++ archives/v1/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Pirates - RPG online gratuit</title>
+	<link rel="stylesheet" type="text/css" href="css/global.css" /> 
+	<link rel="stylesheet" type="text/css" href="css/news.css" /> 
+	<script type="text/javascript" src="js/global.js"></script>
+</head>
+<body>
+<div id="site">
+	<div id="banner">
+		<img alt="Text" title="Fortune et gloire pour les braves" id="text" src="img/banniere/text-pirates-caraibes.png" />
+		<img alt="" title="" id="logo" src="img/banniere/logo-pirates-caraibes.png" />
+	</div>
+	
+	<div id="content">
+		<img id="decor" alt="Palm icon" title="Palme" src="img/feuille_simple_mini.png" />
+		<ul id="tree">
+			<li>News</li>
+			<li><a href="#">Lire</a></li>
+		</ul>
+		
+		<div class="news">
+			<h2>Les registrars</h2>
+			<p>Transfert effectif peut etre un jour, si un jour les gens apprennent à travailler.
+				<br />Transfert repoussé trois fois car ils n avaient pas toutes les pieces alors qu'ils avaient toutes les pieces. Bref j'en ai plein le cul.
+				<br />FUCK
+			</p>
+			<p class="signing"><span class="pseudo">Cap'tain Kid</span>, le 26/09/2008 &agrave; 15:58</p>
+		</div>
+		<div class="news">
+			<h2>Adherer par paypal</h2>
+			<p>Notre compte paypal est bloqué par ces derniers.
+				<br />Vous pouvez toujours adherer mais on ne pourra valider votre adhesion.
+				<br />Le temps que ca se remette en place devrait etre assez long vu que les papiers demandés m'obligent à passer par la prefecture, puis l'insee puis les impôts.
+				<br />Merci et désolé... 
+			</p>
+			<p class="signing"><span class="pseudo">Cap'tain Kid</span>, le 18/09/2008 &agrave; 15:54</p>
+		</div>
+	</div>
+	
+	<dl class="menu">
+		<dt>Connexion</dt>
+			<dd><input id="login" name="form_login" type="text" /></dd>
+			<dd><input id="password" name="form_password" type="text" /></dd>
+	</dl>
+	
+	<dl class="menu">
+		<dt>News</dt>
+			<dd><a href="#">Lire</a></dd>
+		<dt>Jeu</dt>
+			<dd><a href="#">Jouer</a></dd>
+			<dd><a href="#">Histoire</a></dd>
+			<dd><a href="#">R&egrave;gles</a></dd>
+			<dd><a href="#">Statistiques</a></dd>
+			<dd><a href="#">Evolutions</a></dd>
+		<dt>Communaut&eacute;</dt>
+			<dd><a href="#">Association</a></dd>
+			<dd><a href="#">Forum</a></dd>
+			<dd><a href="#">Chat (Gabbly)</a></dd>
+			<dd><a href="#">Guildes</a></dd>
+			<dd><a href="#">Charte</a></dd>
+			<dd><a href="#">Equipe</a></dd>
+		<dt>Culture</dt>
+			<dd><a href="#">Coin Culture</a></dd>
+			<dd><a href="#">R&eacute;cits</a></dd>
+			<dd><a href="#">La BD</a></dd>
+	</dl>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Pirates - RPG online gratuit</title>
+	<link rel="stylesheet" type="text/css" href="css/global.css" /> 
+	<link rel="stylesheet" type="text/css" href="css/news.css" />
+</head>
+<body>
+<div id="site">
+	<div id="banner">
+		<img alt="Title" title="Fortune et gloire pour les braves" id="text" src="pic/banniere/text-pirates-caraibes.png" />
+		<img alt="Logo" title="Logo" id="logo" src="pic/banniere/logo-pirates-caraibes.jpg" />
+	</div>
+	
+	<div id="menus">
+		<dl>
+			<dt>Connexion</dt>
+				<dd>
+					<ul>
+						<li><input id="login" name="form_login" type="text" /></li>
+						<li><input id="password" name="form_password" type="password" /></li>
+					</ul>
+				</dd>
+		</dl>
+		
+		<dl>
+			<dt>News</dt>
+				<dd>
+					<ul>
+						<li><a href="#">Lire</a></li>
+					</ul>
+				</dd>
+			<dt>Jeu</dt>
+				<dd>
+					<ul>
+						<li><a href="#">Jouer</a></li>
+						<li><a href="#">Histoire</a></li>
+						<li><a href="#">R&egrave;gles</a></li>
+						<li><a href="#">Statistiques</a></li>
+						<li><a href="#">Evolutions</a></li>
+					</ul>
+				</dd>
+			<dt>Communaut&eacute;</dt>
+				<dd>
+					<ul>
+						<li><a href="#">Association</a></li>
+						<li><a href="#">Forum</a></li>
+						<li><a href="#">Guildes</a></li>
+						<li><a href="#">Charte</a></li>
+						<li><a href="#">Equipe</a></li>
+					</ul>
+				</dd>
+			<dt>Culture</dt>
+				<dd>
+					<ul>
+						<li><a href="#">Coin Culture</a></li>
+						<li><a href="#">R&eacute;cits</a></li>
+						<li><a href="#">La BD</a></li>
+					</ul>
+				</dd>
+		</dl>
+	</div>
+	
+	
+	<div id="main">
+		<ul id="tree">
+			<li>News</li>
+			<li><a href="#">Lire</a></li>
+		</ul>
+		<img id="decor" alt="Palm icon" title="Palme" src="pic/feuille_simple_mini.png" />
+		
+		<div id="content">
+			<div class="news">
+				<h2>Suivi des &eacute;volutions</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at metus nibh, et varius nunc. In commodo volutpat mi eu condimentum. Donec euismod viverra leo. Etiam interdum interdum varius. Aliquam consectetur varius ante a lacinia. Integer quis ullamcorper dolor. Phasellus velit neque, posuere non mattis iaculis, bibendum et quam. Praesent non nisi arcu. Mauris cursus nulla a eros placerat et porttitor risus porta. In vulputate felis at sapien feugiat gravida iaculis vel libero. Morbi facilisis feugiat justo, et interdum diam scelerisque eget. Morbi at adipiscing arcu. Etiam nec fermentum velit. Morbi pulvinar ipsum vitae leo posuere ac cursus lectus rhoncus. Nulla in dui urna, non consectetur sem.</p>
+				<p class="signing"><span class="pseudo">Cap'tain Derf</span>, le 27/09/2008 &agrave; 23:25</p>
+			</div>
+			<div class="news">
+				<h2>Les registrars</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at metus nibh, et varius nunc. In commodo volutpat mi eu condimentum. Donec euismod viverra leo. Etiam interdum interdum varius. Aliquam consectetur varius ante a lacinia. Integer quis ullamcorper dolor. Phasellus velit neque, posuere non mattis iaculis, bibendum et quam. Praesent non nisi arcu. Mauris cursus nulla a eros placerat et porttitor risus porta. In vulputate felis at sapien feugiat gravida iaculis vel libero. Morbi facilisis feugiat justo, et interdum diam scelerisque eget. Morbi at adipiscing arcu. Etiam nec fermentum velit. Morbi pulvinar ipsum vitae leo posuere ac cursus lectus rhoncus. Nulla in dui urna, non consectetur sem.</p>
+				<p class="signing"><span class="pseudo">Cap'tain Kid</span>, le 26/09/2008 &agrave; 15:58</p>
+			</div>
+			<div class="news">
+				<h2>Adherer par paypal</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at metus nibh, et varius nunc. In commodo volutpat mi eu condimentum. Donec euismod viverra leo. Etiam interdum interdum varius. Aliquam consectetur varius ante a lacinia. Integer quis ullamcorper dolor. Phasellus velit neque, posuere non mattis iaculis, bibendum et quam. Praesent non nisi arcu. Mauris cursus nulla a eros placerat et porttitor risus porta. In vulputate felis at sapien feugiat gravida iaculis vel libero. Morbi facilisis feugiat justo, et interdum diam scelerisque eget. Morbi at adipiscing arcu. Etiam nec fermentum velit. Morbi pulvinar ipsum vitae leo posuere ac cursus lectus rhoncus. Nulla in dui urna, non consectetur sem.</p>
+				<p class="signing"><span class="pseudo">Cap'tain Kid</span>, le 18/09/2008 &agrave; 15:54</p>
+			</div>
+		</div>
+		
+		<p id="footer">&copy; 2004 - 2008 pirates-caraibes.com - Tous droits r&eacute;serv&eacute;s
+		</p>
+	</div>
+	
+</div>
+</body>
+</html>
